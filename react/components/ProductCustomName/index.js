7cc78c6f0a42f768f37d5cc3c1179585228ea600
Ashwath <ashwathnarayana.r@echidnainc.com>
2021-08-02 17:55:19 +0530

    Created Product custom name display from product custom app

---
+++ react/components/ProductCustomName/index.js
@@ -0,0 +1,16 @@
+// Notice that this is TypeScript, and this code should be in a .tsx file
+import React, { FC } from 'react'
+import { useProduct } from 'vtex.product-context'
+
+const ProductCustomName = () => {
+  const productContextValue = useProduct()
+   
+  return (
+    <div>
+      {productContextValue?.product?.productName}
+      {productContextValue?.product?.price};
+    </div>
+  )
+}
+
+export default ProductCustomName
